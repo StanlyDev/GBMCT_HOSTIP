63eaf401bd7c892f236631b0cf5bde90a250bf0a
StanlyDev <stanvsdev@gmail.com>
2024-03-21 20:12:28 -0600

    update

--- mysql.js
+++ mysql.js
@@ -1,28 +1,21 @@
 const mysql = require('mysql');
 
-const conection = mysql.createConnection({
-    host: '10.4.27.79/phpmyadmin',
+// Crear conexión
+const connection = mysql.createConnection({
+    host: 'localhost',
     user: 'root',
     password: 'Stanlyv_00363',
     database: 'gbmct_db'
-})
+});
 
-conection.connect((err)=>{
-    if(err){
-        throw err
-    }
-    else{
-        console.log('Conectado')
-    }
-})
-
+// Conectar a la base de datos
 connection.connect((err) => {
     if (err) {
-      console.error('Error al conectar a la base de datos: ' + err.stack);
-      return;
+        console.error('Error al conectar a la base de datos: ' + err.stack);
+        return;
     }
     console.log('Conexión exitosa a la base de datos con el ID ' + connection.threadId);
-  });
-  
+});
 
-conection.end();
+// Cerrar la conexión después de la conexión exitosa
+connection.end();
