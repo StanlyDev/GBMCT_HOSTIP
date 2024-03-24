1bd56980bc18199d801e529d0e0de8f8c06c7df1
StanlyDev <stanvsdev@gmail.com>
2024-03-23 20:53:01 -0600

    database host implementado, falta conexiones

--- JavaScript/mysql.js
+++ JavaScript/mysql.js
@@ -1,16 +1,14 @@
-// Requerir los módulos necesarios
 const http = require('http');
 const mysql = require('mysql');
 
-// Configurar la conexión a la base de datos
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: '10.4.27.88',
+  user: 'bventura',
   password: 'Stanlyv_00363',
-  database: 'gbmct_db'
+  database: 'gbmct_db',
+  authPlugin: 'mysql_native_password'
 });
 
-// Establecer conexión a la base de datos
 connection.connect((err) => {
   if (err) {
     console.error('Error de conexión:', err);
@@ -19,17 +17,13 @@ connection.connect((err) => {
   console.log('Conexión a la base de datos establecida');
 });
 
-// Crear el servidor HTTP
 const server = http.createServer((req, res) => {
-  // Configurar encabezados CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Request-Method', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
   res.setHeader('Access-Control-Allow-Headers', '*');
 
-  // Verificar el método de solicitud
   if (req.method === 'GET' && req.url === '/data') {
-    // Realizar consulta a la base de datos
     connection.query('SELECT * FROM InventarioCintas', (err, results) => {
       if (err) {
         console.error('Error al ejecutar la consulta:', err);
@@ -38,19 +32,16 @@ const server = http.createServer((req, res) => {
         return;
       }
       
-      // Enviar los resultados como JSON
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(results));
     });
   } else {
-    // Manejar otras rutas o métodos de solicitud
     res.statusCode = 404;
     res.end('Ruta no encontrada');
   }
 });
 
-// Escuchar en un puerto específico
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Servidor en ejecución en http://localhost:${PORT}/`);
