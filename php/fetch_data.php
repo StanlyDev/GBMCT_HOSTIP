78aaeb127271bdaa42132d37be0bc8c484071f86
StanlyDev <stanvsdev@gmail.com>
2024-05-27 00:49:10 -0600

    conect db con php test

---
+++ php/fetch_data.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "10.4.27.113";
+$username = "stanvsdev";
+$password = "Stanlyv_00363";
+$dbname = "dbmedios_gbm";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+$sql = "SELECT NombreCliente, TipoCinta, Descripcion, CodigoCinta, EnCintoteca FROM TableInventory";
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+header('Content-Type: application/json');
+echo json_encode($data);
+?>
