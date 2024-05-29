b53e93d0e7b55b3c68579958e0aac07549e9dcfd
StanlyDev <stanvsdev@gmail.com>
2024-05-29 02:40:51 -0600

    update

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ function login() {
     var password = document.getElementById("password").value;
     var messageElement = document.getElementById("message");
 
-    fetch('/login.php', {
+    fetch('login.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
