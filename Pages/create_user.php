178ff2a7d67a6e20a31cdbea51bffe536a08a9de
StanlyDev <stanvsdev@gmail.com>
2024-05-30 02:43:55 -0600

    update

--- Pages/create_user.php
+++ Pages/create_user.php
@@ -1,12 +1,3 @@
-<?php
-session_start();
-
-// Verificar si el usuario ha iniciado sesión y tiene el rol de "root" o "admin", de lo contrario, redirigirlo a la página de inicio de sesión
-if (!isset($_SESSION["id"]) || ($_SESSION["role"] !== "root" && $_SESSION["role"] !== "admin")) {
-    header("Location: /index.html");
-    exit();
-}
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
