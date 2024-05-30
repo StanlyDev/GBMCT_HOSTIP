c2b465efdcd0edfece9f54a54c9efc5b3319dc4a
StanlyDev <stanvsdev@gmail.com>
2024-05-30 02:55:05 -0600

    change error

--- php/admin_login.php
+++ php/admin_login.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result->num_rows == 1) {
         // Admin encontrado, permitir creación de usuario
         $_SESSION["email"] = $email; // Cambiado de username a email
-        header("Location: /Pages/create_user.php");
+        header("Location: /Pages/create_user.html");
         exit();
     } else {
         // Credenciales incorrectas
