6b994e52e4989ee71a628ec04096cacd908bbcec
StanlyDev <stanvsdev@gmail.com>
2024-05-31 21:34:20 -0600

    update

--- Pages/inventory.php
+++ Pages/inventory.php
@@ -20,6 +20,7 @@ if (!isset($_SESSION["id"])) {
   <script defer src="/JavaScript/BuscadorClient.js"></script>
   <script defer src="/JavaScript/windowsDoc.js"></script>
   <script defer src="/JavaScript/HistoAlert.js"></script>
+  <script defer src="/JavaScript/logout.js"></script>
   <title>GBM | CT</title>
 </head>
 <body>
