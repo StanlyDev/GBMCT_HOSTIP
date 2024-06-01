0af3deb9e96993a74eb49fcb923acb652d755b02
StanlyDev <stanvsdev@gmail.com>
2024-05-31 20:34:22 -0600

    update

--- Pages/IngresoDeMedios.php
+++ Pages/IngresoDeMedios.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+// Verificar si el usuario no ha iniciado sesión, redirigirlo a la página de inicio de sesión
+if (!isset($_SESSION["id"])) {
+    header("Location: /index.html");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
