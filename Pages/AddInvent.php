b3ca01777a43ba572a1c458844388a5acb91813f
StanlyDev <stanvsdev@gmail.com>
2024-06-02 01:10:20 -0600

    udpate

--- Pages/AddInvent.php
+++ Pages/AddInvent.php
@@ -24,8 +24,6 @@ $usuario_rol = $_SESSION["role"] ?? '';
     <title>GBM | CT</title>
 </head>
 <body>
-    <div class="overlay"></div>
-    <!--Inicio Cabecera-->
     <header>
         <div class="menu-btn" onclick="toggleMenu()">☰</div>
         <div class="logo"><img src="/IMG/Logos/Logo-blanco-sin-fondo.png"></div>
@@ -47,9 +45,8 @@ $usuario_rol = $_SESSION["role"] ?? '';
                     style="width:30px;height:30px">
                 </lord-icon></a></div>
         </div>
-        <!--Inicio Menu-->
-        <nav class="navbar"><br>
-            <button class="close-btn" onclick="toggleMenu()">✕</button><br><br>
+        <nav class="navbar">
+            <button class="close-btn" onclick="toggleMenu()">✕</button>
             <ul>
                 <a href="/Pages/inventory.html"><li><img src="/IMG/Icon/box2-fill.svg" style="margin-right: 10px; width: 20px; float: left;">Inventario en Cintoteca</li></a>
                 <a href="#" class="histo" onclick="histoAlert()"><li><img src="/IMG/Icon/arrow-counterclockwise.svg" style="margin-right: 10px; width: 20px; float: left;">Historial I/O</li></a>
@@ -59,73 +56,68 @@ $usuario_rol = $_SESSION["role"] ?? '';
                 <?php endif; ?>            
             </ul>
         </nav>
-        <!--Fin Menu-->
     </header>
-    <!--Fin Cabecera-->
     <main>
-        <!--Inicio Main-->
-        <div class="head-add">
-            <h1>
-                Agregar cintas al Inventario
-            </h1><br>
+        <div class="title_dcgenenator">
+            <h1>Agregar cintas al Inventario</h1>
             <div class="advert">
                 <em><p>INGRESAR TODOS LOS VALORES EN LOS CAMPOS QUE SE PRESENTAN ACONTINUACION</p></em><hr>
             </div>
         </div>
-        <div class="add_info">
+        <div class="doc_info">
             <form action="" id="FrmCinta">
-                <div class="input_container_1">
-                    <div class="client_name">
+                <div class="input_container">
+                    <div class="form-group">
                         <label for="client_name">Nombre del Cliente:</label>
                         <input type="text" id="client_name" name="client_name" placeholder="Client" required>
                     </div>
-                    <div class="co">
-                        <label for="co">Contrato:</label>
-                        <input type="text" placeholder="CO" id="co" class="co" name="co" required>
-                    </div>
-                    <div class="enc">
-                        <label for="enc">Field-Manager:</label>
-                        <input type="text" placeholder="Field-Manager" id="enc" name="enc" required>
-                    </div>
-                </div>
-                <div class="input_container_2">
-                    <div class="HRest">
-                        <label for="hrEsti">Hora de ingreso:</label>
-                        <input type="time" id="hrEsti" name="hrEsti" required>
-                    </div>
-                    <div class="FechaS">
-                        <label for="FechaIO">Fecha de ingreso:</label>
-                        <input type="date" id="FechaIO" name="FechaIO" required>
-                    </div>
-                    <div class="ingr">
-                        <label for="ingr">Agregada por:</label>
-                        <input type="text" id="ingr" name="ingr" placeholder="Operador" required>
-                    </div>
-                </div>
-                <hr>
-                <div class="Info_cinta">
-                    <div class="input_container_3">
-                        <div class="TCinta">
-                            <label for="TypeCinta">Tipo:</label>
-                            <input type="text" class="TipoCint" id="TypeCinta" name="TypeCinta" placeholder="LTO" required>
+                    <div class="form-group">
+                        <label for="co">Contrato:</
+                            label>
+                            <input type="text" placeholder="CO" id="co" class="co" name="co" required>
                         </div>
-                        <div class="Desc">
-                            <label for="DesCin">Descripcion:</label>
-                            <input type="text" id="DesCin" name="DesCin" placeholder="LTO - 1.5TB" required>
+                        <div class="form-group">
+                            <label for="enc">Email Field Manager:</label>
+                            <input type="text" placeholder="Field-Manager" id="enc" name="enc" required>
                         </div>
-                        <div class="Code">
-                            <label for="CCinta">Codigo:</label>
-                            <input type="text" id="CCinta" name="CCinta" placeholder="CODE0001" required>
-                        </div>
-                    </div><br>
-                    <div class="AgreBtn">
-                        <button style="margin-right: 20px;" type="submit" onclick="agregarCinta()">
-                            <img src="/IMG/Icon/plus-lg.svg"> Agregar
-                        </button>
                     </div>
-                </div>
-            </form>
-            <div class="List_Preview">
+                    <div class="input_container">
+                        <div class="form-group">
+                            <label for="hrEsti">Hora de ingreso:</label>
+                            <input type="time" id="hrEsti" name="hrEsti" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="FechaIO">Fecha de ingreso:</label>
+                            <input type="date" id="FechaIO" name="FechaIO" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="ingr">Agregada por:</label>
+                            <input type="text" id="ingr" name="ingr" placeholder="Operador" required>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="Info_cinta">
+                        <div class="input_container">
+                            <div class="form-group">
+                                <label for="TypeCinta">Tipo:</label>
+                                <input type="text" class="TipoCint" id="TypeCinta" name="TypeCinta" placeholder="LTO" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="DesCin">Descripcion:</label>
+                                <input type="text" id="DesCin" name="DesCin" placeholder="LTO - 1.5TB" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="CCinta">Codigo:</label>
+                                <input type="text" id="CCinta" name="CCinta" placeholder="CODE0001" required>
+                            </div>
+                        </div><br>
+                        <div class="AgreBtn">
+                            <button type="submit" onclick="agregarCinta()">
+                                <img src="/IMG/Icon/plus-lg.svg"> Agregar
+                            </button>
+                        </div>
+                    </div>
+                </form>
                 <div class="tabla_Cont">
                     <div class="tabla_preview">
                         <table id="tablaCintas">
@@ -136,39 +128,34 @@ $usuario_rol = $_SESSION["role"] ?? '';
                                 <th>Descripcion</th>
                                 <th>Codigo</th>
                             </tr>
-                            <!-- ... Contenido de la tabla ... -->
+                            <!-- Contenido de la tabla -->
                         </table>
                     </div>
-            </div><br>
-          </div>
+                </div>
+            </div>
+        </main>
+        <div id="myModal" class="modal">
+            <div class="modal-content">
+                <span class="close" onclick="closeOptions()">&times;</span>
+                <p>Elige una opción:</p><hr>
+                <a href="/Pages/IngresoDeMedios.php">Ingreso de Medios</a>
+                <a href="/Pages/SalidaDeMedios.php">Salida de Medios</a>
+            </div>
         </div>
-        <!--Fin Main-->
-    </main>
-    <!-- Ventana emergente -->
-    <div id="myModal" class="modal">
-        <div class="modal-content">
-            <span class="close" onclick="closeOptions()">&times;</span>
-            <p>Elige una opción:</p><hr>
-            <a href="/Pages/IngresoDeMedios.php">Ingreso de Medios</a>
-            <a href="/Pages/SalidaDeMedios.php">Salida de Medios</a>
+        <footer>
+            <div class="botones-container">
+                <a href="#">
+                    <button onclick="">
+                        <img style="height: 20px;" src="/IMG/Icon/database-add.svg">
+                    </button>                
+                </a>
+            </div>
+        </footer>
+        <div id="inactivityModal" class="modal">
+            <div class="modal-content">
+                <p>En <span id="inactivityCountdown">25</span> segundos se cerrará la sesión.</p>
+                <button id="continueSessionBtn">Continuar sesión</button>
+            </div>
         </div>
-    </div>
-    <footer>
-        <div class="botones-container">
-            <a href="#">
-                <button onclick="">
-                    <img src="/IMG/Icon/database-add.svg">
-                    <img src="/IMG/Icon/database-fill-add.svg">
-                </button>                
-            </a>
-        </div>
-    </footer>
-    <div id="inactivityModal" class="modal">
-    <div class="modal-content">
-        <p>En <span id="inactivityCountdown">25</span> segundos se cerrará la sesión.</p>
-        <button id="continueSessionBtn">Continuar sesión</button>
-    </div>
-</div>
-</body>
-<!--Devoloped by Brandon Ventura-->
-</html>
+    </body>
+    </html>
