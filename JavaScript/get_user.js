119e191a6c1e0bee0b3819ddd3be491ffd8b7338
StanlyDev <stanvsdev@gmail.com>
2024-06-02 02:12:14 -0600

    update

--- JavaScript/get_user.js
+++ JavaScript/get_user.js
@@ -21,3 +21,9 @@ function cargarUsuarios() {
 
 // Función para cargar usuarios al cargar la página
 window.onload = cargarUsuarios;
+
+// Función para editar usuario
+function editarUsuario(userId) {
+    // Aquí puedes implementar la lógica para editar un usuario
+    console.log('Editando usuario con ID:', userId);
+}
