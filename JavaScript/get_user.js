57db15a2a7950a2cb3182ebbb9a0485abd22e55d
StanlyDev <stanvsdev@gmail.com>
2024-06-02 02:57:09 -0600

    update

--- JavaScript/get_user.js
+++ JavaScript/get_user.js
@@ -2,11 +2,29 @@
 function getUsers() {
     // Realizar una solicitud AJAX para obtener los usuarios
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/php/get_user.php", true);
+    xhr.open("GET", "/php/get_users.php", true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            // Actualizar el cuerpo de la tabla con los usuarios obtenidos
-            document.getElementById("userTableBody").innerHTML = xhr.responseText;
+            // Parsear la respuesta JSON
+            var users = JSON.parse(xhr.responseText);
+
+            // Obtener la tabla de usuarios
+            var userTable = document.getElementById("userTableBody");
+
+            // Limpiar la tabla antes de agregar usuarios
+            userTable.innerHTML = "";
+
+            // Iterar sobre los usuarios y agregarlos a la tabla
+            for (var i = 0; i < users.length; i++) {
+                var user = users[i];
+                var row = "<tr>";
+                row += "<td>" + user.username + "</td>";
+                row += "<td>" + user.email + "</td>";
+                row += "<td>" + user.role + "</td>";
+                row += "<td><button onclick='editUser(" + user.id + ")'>Editar</button></td>";
+                row += "</tr>";
+                userTable.innerHTML += row;
+            }
         }
     };
     xhr.send();
@@ -14,3 +32,9 @@ function getUsers() {
 
 // Llamar a la función para obtener los usuarios cuando la página se carga
 window.onload = getUsers;
+
+// Función para editar un usuario
+function editUser(userId) {
+    // Redirigir a la página de edición de usuario con el ID del usuario como parámetro
+    window.location.href = "/Pages/edit_user.php?id=" + userId;
+}
