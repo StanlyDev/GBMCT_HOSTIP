9b6ca3cfee60fe16fe1a4f76d5856dddd681a0eb
StanlyDev <stanvsdev@gmail.com>
2024-06-11 03:51:52 -0600

    update

--- php/delete_cinta.php
+++ php/delete_cinta.php
@@ -18,16 +18,24 @@ if ($conn->connect_error) {
 if (isset($_POST['id_cinta'])) {
     $id_cinta = $_POST['id_cinta'];
 
-    // Preparar y ejecutar consulta SQL para eliminar la cinta
-    $sql = "DELETE FROM TableInventory WHERE NumeroCinta = $id_cinta";
-    if ($conn->query($sql) === TRUE) {
+    // Preparar la consulta SQL con una sentencia preparada
+    $sql = "DELETE FROM TableInventory WHERE NumeroCinta = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id_cinta); // "i" indica que el parámetro es un entero
+
+    // Ejecutar la consulta preparada
+    if ($stmt->execute()) {
         echo "La cinta ha sido eliminada correctamente";
     } else {
-        echo "Error al eliminar la cinta: " . $conn->error;
+        echo "Error al eliminar la cinta: " . $stmt->error;
     }
+
+    // Cerrar la sentencia preparada
+    $stmt->close();
 } else {
     echo "No se proporcionó un ID de cinta para eliminar";
 }
 
+// Cerrar conexión
 $conn->close();
 ?>
