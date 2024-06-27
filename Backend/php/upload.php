e2672755720e7ef5f3c82bb24575e6b7424a6dc8
StanlyDev <stanvsdev@gmail.com>
2024-06-27 02:02:01 -0600

    update

--- Backend/php/upload.php
+++ Backend/php/upload.php
@@ -9,32 +9,44 @@ try {
     // Verifica si se ha enviado un archivo
     if (!isset($_FILES["file"])) {
         throw new Exception("No se ha recibido ningún archivo.");
+        header("Location: /Frontend/Pages/UpdateFile.php");
+        exit();
     }
 
     // Verifica si hubo errores en la subida
     if ($_FILES["file"]["error"] != UPLOAD_ERR_OK) {
         throw new Exception("Error al subir el archivo.");
+        header("Location: /Frontend/Pages/UpdateFile.php");
+        exit();
     }
 
     // Verifica si el archivo ya existe
     if (file_exists($target_file)) {
         throw new Exception("El archivo ya existe.");
+        header("Location: /Frontend/Pages/UpdateFile.php");
+        exit();
     }
 
     // Verifica el tamaño del archivo (máximo 5MB en este ejemplo)
     if ($_FILES["file"]["size"] > 5000000) {
         throw new Exception("El archivo es demasiado grande.");
+        header("Location: /Frontend/Pages/UpdateFile.php");
+        exit();
     }
 
     // Permitir ciertos formatos de archivo (opcional)
     $allowed_types = array("jpg", "png", "jpeg", "gif", "pdf", "docx", "xlsx", "xlsm");
     if (!in_array($imageFileType, $allowed_types)) {
         throw new Exception("Solo se permiten archivos JPG, JPEG, PNG, GIF, PDF, DOCX, XLSM y XLSX.");
+        header("Location: /Frontend/Pages/UpdateFile.php");
+        exit();
     }
 
     // Intenta mover el archivo subido al directorio de destino
     if (!move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
         throw new Exception("Error al mover el archivo al directorio de destino.");
+        header("Location: /Frontend/Pages/UpdateFile.php");
+        exit();
     }
 
     // Éxito al subir el archivo
