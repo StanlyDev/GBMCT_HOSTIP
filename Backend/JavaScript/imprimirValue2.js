4ec4340f78d8e9854c75660193846e672dbe2ecb
StanlyDev <stanvsdev@gmail.com>
2024-07-04 02:25:53 -0600

    up

--- Backend/JavaScript/imprimirValue2.js
+++ Backend/JavaScript/imprimirValue2.js
@@ -36,26 +36,26 @@ document.addEventListener('DOMContentLoaded', function () {
       numeroTicketElement.textContent = numeroTicketValue;
       deliveredByElement.textContent = nombre1Value + " con DNI " + dni1Value;
       receivedByElement.textContent = nombre2Value + " con DNI " + dni2Value;
-
-      var tablaDocVisor = document.querySelector(".Table_Cinta table");
-
-      if (tablaDocVisor) {
-          tablaDocVisor.innerHTML = "<tr><th>#</th><th>Cliente</th><th>Tipo</th><th>Descripcion</th><th>Codigo</th><th>Ubicacion</th></tr>";
-
-          datosCompartidos.cintas.forEach(function (cinta, index) {
-              var fila = tablaDocVisor.insertRow(-1);
-              var propiedadesCinta = ['numero', 'cliente', 'tipo', 'descripcion', 'codigo', 'ubicacion'];
-
-              propiedadesCinta.forEach(function (propiedad) {
-                  var celda = fila.insertCell();
-                  celda.textContent = cinta[propiedad];
-              });
-          });
-      } else {
-          console.error("La tabla de documentos no fue encontrada en el documento.");
-      }
   } else {
       console.error("Uno o más elementos no fueron encontrados en el documento.");
+  }
+
+  var tablaDocVisor = document.querySelector(".Table_Cinta table");
+
+  if (tablaDocVisor) {
+      tablaDocVisor.innerHTML = "<tr><th>#</th><th>Cliente</th><th>Tipo</th><th>Descripcion</th><th>Codigo</th><th>Ubicacion</th></tr>";
+
+      datosCompartidos.cintas.forEach(function (cinta, index) {
+          var fila = tablaDocVisor.insertRow(-1);
+          var propiedadesCinta = ['numero', 'cliente', 'tipo', 'descripcion', 'codigo', 'ubicacion'];
+
+          propiedadesCinta.forEach(function (propiedad) {
+              var celda = fila.insertCell();
+              celda.textContent = cinta[propiedad];
+          });
+      });
+  } else {
+      console.error("La tabla de documentos no fue encontrada en el documento.");
   }
 });
 
