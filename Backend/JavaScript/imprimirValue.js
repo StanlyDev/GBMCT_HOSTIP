3d0c3184375a1d9a3e5d5ce98d758d8ef6421008
StanlyDev <stanvsdev@gmail.com>
2024-07-04 01:12:35 -0600

    udpate

--- Backend/JavaScript/imprimirValue.js
+++ Backend/JavaScript/imprimirValue.js
@@ -33,23 +33,19 @@ document.addEventListener('DOMContentLoaded', function () {
   var tablaDocVisor = document.querySelector(".Table_Cinta table");
 
   // Limpiar la tabla antes de agregar nuevos datos
-  tablaDocVisor.innerHTML = "<tr><th>#</th><th>Cliente</th><th>Tipo</th><th>Descripcion</th><th>Codigo</th><th>Ubicacion</th></tr>";
+  tablaDocVisor.innerHTML = "<tr><th>#</th><th>Cliente</th><th>Tipo</th><th>Descripcion</th><th>Código</th><th>Ubicacion</th></tr>";
 
   // Iterar sobre los datos compartidos y agregar filas a la tabla
   datosCompartidos.cintas.forEach(function (cinta, index) {
     var fila = tablaDocVisor.insertRow(-1);
 
     // Asegurarse de que estas propiedades coincidan con las que usaste en script.js
-    var propiedadesCinta = ['numero', 'cliente', 'tipo', 'descripcion', 'codigo', 'ubicacion'];
+    var propiedadesCinta = ['numero', 'cliente',  'tipo', 'descripcion', 'codigo', 'ubicacion'];
 
     propiedadesCinta.forEach(function (propiedad) {
       var celda = fila.insertCell();
       celda.textContent = cinta[propiedad];
     });
-
-    // Agregar la ubicación al final de cada fila
-    var ubicacionCell = fila.insertCell();
-    ubicacionCell.textContent = cinta.ubicacion;
   });
 });
 
@@ -57,3 +53,5 @@ function getParameterByName(name) {
   var urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(name);
 }
+
+/*Developed by Brandon Ventura*/
