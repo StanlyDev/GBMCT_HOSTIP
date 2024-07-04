3074f5d41e5e4df0389dfa4aee4bb49da4806fdc
StanlyDev <stanvsdev@gmail.com>
2024-07-04 03:00:59 -0600

    update

--- Backend/JavaScript/imprimirValue2.js
+++ Backend/JavaScript/imprimirValue2.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var origenDocumentoValue = getParameterByName('origen');
   var destinoDocumentoValue = getParameterByName('destino');
   var horaEstimadaValue = getParameterByName('hrEsti');
-  var nombreClienteValue = getParameterByName('destino');
+  var nombreClienteValue = getParameterByName('origen');
   var solicitadoPorValue = getParameterByName('soliX');
   var numeroTicketValue = getParameterByName('sr');
   var nombre1Value = sessionStorage.getItem("nombre1");
