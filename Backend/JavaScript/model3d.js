b9e0e3140a731accaf42f037ce60781d9a421e9b
StanlyDev <stanvsdev@gmail.com>
2024-07-07 22:53:29 -0600

    new script 3d objet

--- Backend/JavaScript/model3d.js
+++ Backend/JavaScript/model3d.js
@@ -5,29 +5,20 @@ import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/l
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
-let isDragging = false;
-
 let object;
-
-let controls;
-
-let objToRender = 'ibm';
-let rotationSpeed = 0.005;
-
-let initialRotationX = 0;
-let initialRotationY = 0;
 
 const loader = new GLTFLoader();
 loader.load(
   '/Frontend/3D/ibm_1401/scene.gltf',
   function (gltf) {
     object = gltf.scene;
-    object.scale.set(5, 5, 5);
-    initialRotationX = object.rotation.x;
-    initialRotationY = object.rotation.y;
+    object.scale.set(2.5, 2.5, 2.5);
+
+    object.position.set(0, -100, 0);
+
     scene.add(object);
+
+    controls.target.set(0, 0, 0);
   },
   function (xhr) {
     console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -43,25 +34,35 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const container = document.getElementById("container3D");
 container.appendChild(renderer.domElement);
 
-camera.position.z = 800;
-camera.position.x = 0;
-camera.position.y = 0;
+camera.position.set(0, 0, 380);
 
-const topLight = new THREE.DirectionalLight(0xffffff, 1);
-topLight.position.set(500, 500, 500);
-topLight.castShadow = true;
-scene.add(topLight);
+const lights = [];
+lights[0] = new THREE.DirectionalLight(0xffffff, 1);
+lights[0].position.set(1, 1, 1);
+scene.add(lights[0]);
+
+lights[1] = new THREE.DirectionalLight(0xffffff, 0.5);
+lights[1].position.set(-1, 1, -1);
+scene.add(lights[1]);
+
+lights[2] = new THREE.DirectionalLight(0xffffff, 0.5);
+lights[2].position.set(-1, -1, 1);
+scene.add(lights[2]);
+
+lights[3] = new THREE.DirectionalLight(0xffffff, 0.5);
+lights[3].position.set(1, -1, -1);
+scene.add(lights[3]);
 
 const ambientLight = new THREE.AmbientLight(0x333333, 1);
 scene.add(ambientLight);
 
-controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+controls.target.set(0, 0, 0);
 
 function animate() {
   requestAnimationFrame(animate);
-  if (object && !isDragging) {
-    object.rotation.y += rotationSpeed;
-  }
+  controls.update();
   renderer.render(scene, camera);
 }
 
@@ -69,13 +70,6 @@ window.addEventListener("resize", function () {
   camera.aspect = container.clientWidth / container.clientHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(container.clientWidth, container.clientHeight);
-});
-
-renderer.domElement.addEventListener("mousedown", () => {
-  isDragging = true;
-});
-renderer.domElement.addEventListener("mouseup", () => {
-  isDragging = false;
 });
 
 animate();
