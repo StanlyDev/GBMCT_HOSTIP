1d7bac1cbeac7359faa3f0973a4c4c5c17843385
StanlyDev <stanvsdev@gmail.com>
2024-07-09 23:38:08 -0600

    CHANGE_PASS CHANGE MIN

--- login.php
+++ login.php
@@ -1,53 +1,69 @@
 <?php
 session_start();
 
-$servername = "10.4.27.113";
+$servername = "10.4.27.116";
 $username = "stanvsdev";
-$password = "Stanlyv_00363";
+$password = "Stanlyv00363";
 $dbname = "dbmedios_gbm";
 
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-
-// Inicializar mensaje de error vacío
-$errorMsg = "";
-
-// Verificar si se enviaron datos de inicio de sesión
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+
+    $errorMsg = "";
+    $email = $conn->real_escape_string($_POST["email"]);
     $password = $_POST["password"];
 
-    // Escapar caracteres especiales para evitar inyección SQL
-    $username = $conn->real_escape_string($username);
-    $password = $conn->real_escape_string($password);
-
-    // Consulta para buscar el usuario en la tabla 'usuarios'
-    $sql = "SELECT id, username, role FROM usuarios WHERE username='$username' AND password='$password'";
+    $sql = "SELECT id, username, email, role, password, first_login FROM usuarios WHERE email='$email'";
     $result = $conn->query($sql);
 
     if ($result->num_rows == 1) {
-        // Usuario encontrado, iniciar sesión
         $row = $result->fetch_assoc();
-        $_SESSION["id"] = $row["id"];
-        $_SESSION["username"] = $row["username"];
-        $_SESSION["role"] = $row["role"];
 
-        // Redireccionar a la página de inicio o a donde sea necesario
-        header("Location: /Frontend/Pages/HomePage.html");
-        exit();
+        if ($password == "GBM123") {
+            // Contraseña predeterminada encontrada, iniciar sesión y redirigir a cambiar contraseña
+            $_SESSION["id"] = $row["id"];
+            $_SESSION["email"] = $row["email"];
+            $_SESSION["role"] = $row["role"];
+            $_SESSION["username"] = $row["username"];
+            $_SESSION["first_login"] = true; // Marcar como primer inicio de sesión
+
+            header("Location: /Pages/change_password.php");
+            exit();
+        } else {
+            $stored_hash = $row["password"];
+            if (password_verify($password, $stored_hash)) {
+                $_SESSION["id"] = $row["id"];
+                $_SESSION["email"] = $row["email"];
+                $_SESSION["role"] = $row["role"];
+                $_SESSION["username"] = $row["username"];
+
+                if ($row["first_login"] == 1) {
+                    $_SESSION["first_login"] = true;
+                    header("Location: /Frontend/Pages/change_password.php");
+                    exit();
+                } else {
+                    header("Location: /Frontend/Pages/HomePage.php");
+                    exit();
+                }
+            } else {
+                $_SESSION["errorMsg"] = "Usuario o contraseña incorrectos";
+                header("Location: /index.html");
+                exit();
+            }
+        }
     } else {
-        // Usuario no encontrado, establecer mensaje de error
         $_SESSION["errorMsg"] = "Usuario o contraseña incorrectos";
-        header("Location: /index.html"); // Redireccionar al formulario de inicio de sesión
+        header("Location: /index.html");
         exit();
     }
-}
 
-// Cerrar conexión MySQL
-$conn->close();
+    $conn->close();
+} else {
+    header("Location: /index.html");
+    exit();
+}
 ?>
