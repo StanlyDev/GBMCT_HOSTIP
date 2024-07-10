409007219bdf4546c14798cedfc47ab2a25d0b5b
StanlyDev <stanvsdev@gmail.com>
2024-07-09 22:59:37 -0600

    add search with code internal

--- Backend/JavaScript/BuscadorClient.js
+++ Backend/JavaScript/BuscadorClient.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Obtener elementos del DOM
     var inputBusqueda = document.getElementById("search-input");
     var inputBusquedaCodigo = document.getElementById("search-codigo");
+    var inputBusquedaCodigoInter = document.getElementById("search-codigo-inter");
     var checkboxes = document.querySelectorAll('.checkend_inv input[type="checkbox"]');
     var tabla = document.querySelector(".tabla_invent table");
     var filas = tabla.getElementsByTagName("tr");
@@ -17,17 +18,20 @@ document.addEventListener("DOMContentLoaded", function () {
     function realizarBusqueda() {
         var busqueda = inputBusqueda.value.toLowerCase();
         var busquedaCodigo = inputBusquedaCodigo.value.toLowerCase();
+        var busquedaCodigoInter = inputBusquedaCodigoInter.value.toLowerCase();
 
         for (var i = 1; i < filas.length; i++) {
             var fila = filas[i];
             var nombreCliente = fila.getElementsByTagName("td")[1].innerText.toLowerCase();
             var codigoCinta = fila.getElementsByTagName("td")[4].innerText.toLowerCase();
-            var enCintoteca = fila.getElementsByTagName("td")[5].innerText.toLowerCase();
+            var codigoCinInter = fila-getElementsByTagName("td")[5].innerText.toLowerCase();
+
 
             var mostrarFila = true;
 
             if ((busqueda && !nombreCliente.includes(busqueda)) ||
-                (busquedaCodigo && !codigoCinta.includes(busquedaCodigo))) {
+                (busquedaCodigo && !codigoCinta.includes(busquedaCodigo)) || 
+                (busquedaCodigoInter && !codigoCinInter.includes(busquedaCodigoInter))) {
                 mostrarFila = false;
             }
 
@@ -50,6 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Agregar evento de entrada al input de búsqueda
     inputBusqueda.addEventListener("input", realizarBusqueda);
     inputBusquedaCodigo.addEventListener("input", realizarBusqueda);
+    inputBusquedaCodigoInter.addEventListener("input", realizarBusqueda);
 
     // Asignar automáticamente el número de cliente a las filas existentes
     asignarNumerosSecuenciales();
