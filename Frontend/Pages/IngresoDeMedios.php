458848f0808bce5f2887deacb82bb02936180e8b
StanlyDev <stanvsdev@gmail.com>
2024-07-16 18:19:53 -0600

    secure disable temp for testing mail send

--- Frontend/Pages/IngresoDeMedios.php
+++ Frontend/Pages/IngresoDeMedios.php
@@ -24,7 +24,6 @@ $usuario_rol = $_SESSION["role"] ?? '';
     <script defer src="/Backend/JavaScript/firmas.js"></script>
     <script defer src="/Backend/JavaScript/logout.js"></script>
     <script defer src="/Backend/JavaScript/script.js"></script>
-    <script defer src="/Backend/JavaScript/secure.js"></script>
     <title>GBM | CT</title>
     <script>
         function enviarCorreo() {
